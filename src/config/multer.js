9a7f2f238c81be2e9e42359e7ecf87a4a00d1f28
Rakesh Prajapati <mp966904@gmail.com>
2026-05-13 01:09:39 +0530

    Fixed uploads folder issue

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,17 +1,23 @@
 import multer from "multer";
 import path from "path";
-import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
 
 const storage = multer.diskStorage({
 
   destination: function (req, file, cb) {
+
     cb(null, "uploads/");
   },
 
   filename: function (req, file, cb) {
 
     const uniqueName =
-      uuidv4() + path.extname(file.originalname);
+      Date.now() +
+      path.extname(file.originalname);
 
     cb(null, uniqueName);
   },
